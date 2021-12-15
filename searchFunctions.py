a7bbf0259343b2309136311be5b5dffc9fbba4fd
KonstantinLiehr <54676578+KonstantinLiehr@users.noreply.github.com>
2021-12-15 00:55:45 -0500

    Updated searchActor function with stored procedure
    Added instructions on how to use stored procedures in gcp

--- searchFunctions.py
+++ searchFunctions.py
@@ -1,13 +1,28 @@
 import pymysql.cursors
 
 def searchActors(values, cur):
-    # server connect
 
+    #This line checks if the person's name is in the person list in the database (need to change to just actor index)
     cur.execute("select * from person where person_name = (%s)", (values[0]))
+    #It takes the first row, if the row is empty ( == None) then it moves on, if the person's name is in the database
+    #it does the stored procedure
     row = cur.fetchone()
     if (row != None):
-        values[0] = """select person_name, title from
-          movie as c join
-          (select person_name, movie_id from movie_cast as a join person as b on a.person_id = b.person_id where person_name = \"""" + values[0] + """\")
-          as d on c.movie_id = d.movie_id;"""
+        values[0] = "CALL SearchActors('" + values[0] + "');"
+    
+    #How to I created the stored procedure in gcp: 
+    #Use this link for reference its super good https://dev.mysql.com/doc/refman/8.0/en/create-procedure.html
+    #My stored procedure for SearchActors:
+    #    mysql> delimiter //
+    #    mysql> CREATE PROCEDURE SearchActors (IN Name varchar(40)) 
+    #    BEGIN 
+    #        select person_name, title, character_name, overview, runtime, popularity from movie as c join 
+    #        (select person_name, movie_id, character_name from movie_cast as a join person as b on a.person_id = b.person_id where person_name = Name) 
+    #        as d on c.movie_id = d.movie_id; 
+    #    END//
+    #    mysql> delimiter ;  
+
+    #Command use to check if your stored procedure was created:
+    #    mysql> select routine_name, routine_type,definer,created,security_type,SQL_Data_Access from information_schema.routines where routine_type='PROCEDURE' and routine_schema='movies';
+
     return values
