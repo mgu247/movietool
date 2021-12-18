572873a8dcf00ec5b54e1e158c1287b6dada1f7f
jh-lee3339 <84605635+jh-lee3339@users.noreply.github.com>
2021-12-17 21:41:36 -0500

    added new stored procedure

--- searchFunctions.py
+++ searchFunctions.py
@@ -10,7 +10,7 @@ def searchActors(values, cur):
     row = cur.fetchone()
     if (row != None):
         values[0] = "CALL SearchActors('" + values[0] + "');"
-
+   
     #How to I created the stored procedure in gcp:
     #Use this link for reference its super good https://dev.mysql.com/doc/refman/8.0/en/create-procedure.html
     #My stored procedure for SearchActors:
@@ -23,7 +23,25 @@ def searchActors(values, cur):
     #    END//
     #    mysql> delimiter ;
 
+
+
     #Command use to check if your stored procedure was created:
     #    mysql> select routine_name, routine_type,definer,created,security_type,SQL_Data_Access from information_schema.routines where routine_type='PROCEDURE' and routine_schema='movies';
 
     return values
+
+    def searchTitle(values, cur):
+        cur.execute("select * from movie where title = (%s)", (values[0]))
+        row = cur.fetchone()
+        if (row != None):
+        values[0] = "CALL SearchTitle('" + values[0] + "');"
+        
+        # Stored procedure for SearchTitle:
+        # 
+        #   mysql> CREATE PROCEDURE SearchTitle (IN Name varchar(1000))
+        #   BEGIN
+        #   select title, release_date, popularity, runtime, budget, movie_status from movie where title = Name;
+        #   END
+        #
+
+        return values
